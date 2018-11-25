d67a554ad594b75730c921aafa8d4b74017b3233
Claudio Amaral <claudio.amaral@stf.jus.br>
2018-11-24 23:50:38 -0200

    Inclusão do arquivo index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+/*
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+var http = require('http');
+var url = require('url');
+
+ // Configure the server to respond to all requests with a string
+var server = http.createServer(function(req,res){
+
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Checks the route and sets the message
+  var statusCode;
+  var message;
+  if (trimmedPath == 'hello') {
+    statusCode = 200;
+    message = '{"message":"Hello World!"}';
+  } else {
+    statusCode = 404;
+    message = '{}';
+  }
+
+  // Send the response
+  res.setHeader('Content-Type', 'application/json');
+  res.writeHead(statusCode);
+  res.end(message);
+
+  // Log the request/response
+  console.log('Request received on path: '+trimmedPath);
+});
+
+// Start the server
+server.listen(3000,function(){
+  console.log('The server is up and running now');
+});
